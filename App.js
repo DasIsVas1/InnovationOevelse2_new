3c743b9cd4149851db4bce296763e37f60b23ae9
dasisvas1 <krva18ab@student.cbs.dk>
2020-09-20 12:18:35 +0200

    Basic "home" og "settings" screen lavet og importeret i app.js

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./components/Home";
+import Settings from "./components/Settings";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Øvelse 3</Text>
       <StatusBar style="auto" />
+
+      <Home/>
+
+      <Settings/>
+
+
     </View>
   );
 }
---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import View from "react-native-web/dist/exports/View";
+import Text from "react-native-web/dist/exports/Text";
+
+export default class Home extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Dette er Home Screen </Text>
+            </View>
+        )
+    }
+}
